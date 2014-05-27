ea16454feb1ee100d0e61d6b3f86e833c8d5a581
Nathan Vander Wilt <nate@calftrail.com>
2014-05-27 13:49:25 -0700

    fix wrapper trouble

--- index.js
+++ index.js
@@ -20,20 +20,20 @@ module.exports = function SerialQueue() {
     // (pass `true` for `nested` if your own caller already holds lock)
     q.TRANSACTION_WRAPPER = function (cb, proceed, nested) {
         if (nested) return process.nextTick(proceed), cb;
-        
         var _releaseQueue;
         if ('postAcquire' in this) proceed = this.postAcquire.bind(q, proceed);
         q.acquire(function (releaseQueue) {
             _releaseQueue = releaseQueue;
             proceed();
         });
-        var finish = ('preRelease' in this) ? this.preRelease.bind(q, _finish) : _finish;
-        function _finish(applyCB) {
-            applyCB();
+        var capturedCB, finish = ('preRelease' in this) ? this.preRelease.bind(q, _finish) : _finish;
+        function _finish() {
+            capturedCB();
             _releaseQueue();
         }
         return function () {
-            _finish.call(q, Function.prototype.apply.bind(cb, this, arguments), Array.prototype.slice.call(arguments, 0), this);
+            capturedCB = Function.prototype.apply.bind(cb, this, arguments);
+            finish.call(null, Array.prototype.slice.call(arguments, 0), this);
         };
     };
     
